828ec9481636a13c5bf28378ed5889da06dffba4
kuntaa6 <Muthokilucas@gmail.com>
2022-06-30 14:17:41 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
-int main(){
-
-
-
-
-puts("\"Programming is like building a multilingual puzzle");
-return 0
+/**
+ * main - prints exactly the words
+ *
+ * Return : 0
+ */
+int main(void)
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	returns (0);
 }
