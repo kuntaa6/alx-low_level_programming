1d66cd8194026fa1826fba15f097960ec6a90bf2
kuntaa6 <Muthokilucas@gmail.com>
2022-07-03 07:19:28 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - prints exactly the words
  *
- * Return : 0
+ * Return: 0 when succesful
  */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	returns (0);
+	return (0);
 }
